2d1247871691b6353e65a92954258b3afa726c05
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2018-07-16 15:18:13 +0200

    Rename attribute to better reflect its purpose.

---
+++ Enigmatry.Blueprint.Api/Filters/CancelSavingTransactionAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Enigmatry.Blueprint.Core.Data;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Enigmatry.Blueprint.Api.Filters
+{
+    internal class CancelSavingTransactionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            CancelSavingIfModelInvalid(context);
+         }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            CancelSavingIfModelInvalid(context);
+         }
+
+        private static void CancelSavingIfModelInvalid(FilterContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var unitOfWork = context.HttpContext.Resolve<IUnitOfWork>();
+                unitOfWork.CancelSaving();
+            }
+        }
+    }
+}
--- Enigmatry.Blueprint.Api/Filters/TransactionFilterAttribute.cs
+++ Enigmatry.Blueprint.Api/Filters/TransactionFilterAttribute.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Enigmatry.Blueprint.Core.Data;
 using Microsoft.AspNetCore.Mvc.Filters;
 
