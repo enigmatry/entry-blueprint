9579f2f6bcdd7591447b05c467094a7d9abee982
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2018-08-28 16:00:59 +0200

    Fix sonarcube warning.

--- enigmatry-blueprint-app/Startup.cs
+++ enigmatry-blueprint-app/Startup.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +11,7 @@ namespace Enigmatry.Blueprint.App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+          // this app is empty
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
