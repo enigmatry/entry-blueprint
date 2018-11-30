cfcb70a7fbea14b0e43a08c4408f7a33a8421e98
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2018-11-30 12:04:02 +0100

    Switch to scss instead of css.

--- enigmatry-blueprint-app/src/app/app.module.ts
+++ enigmatry-blueprint-app/src/app/app.module.ts
@@ -1,13 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
