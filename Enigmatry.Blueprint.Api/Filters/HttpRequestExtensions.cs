9b555b5eb398b5215e93abbbfe86273f14c062d6
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2018-12-04 14:38:14 +0100

    Added missing class

---
+++ Enigmatry.Blueprint.Api/Filters/HttpRequestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Enigmatry.Blueprint.Api.Filters
+{
+    public static class HttpRequestExtensions
+    {
+        public static bool IsTrusted(this HttpRequest request, bool useDeveloperExceptionPage)
+        {
+            // TODO add logic if for certain users/roles you want to return error details in the response
+            return useDeveloperExceptionPage;
+        }
+    }
+}
