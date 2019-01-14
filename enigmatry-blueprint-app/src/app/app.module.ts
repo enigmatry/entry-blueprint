11f4c3860b1edc2bade496a12de2252327812de4
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2019-01-14 15:53:11 +0100

    BP-53: Added ngx-translate-messageformat-compiler and messageformat as explained here: https://www.codeandweb.com/babeledit/tutorials/how-to-translate-your-angular7-app-with-ngx-translate Dynamic parameters in translation files must not use {{}} syntax but {} instead

--- enigmatry-blueprint-app/src/app/app.module.ts
+++ enigmatry-blueprint-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateCompiler, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
+import { MESSAGE_FORMAT_CONFIG } from 'ngx-translate-messageformat-compiler';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,19 +15,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    TranslateModule.forRoot(),
     AccordionModule.forRoot(),
     BsDatepickerModule.forRoot(),
     HttpClientModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        })
+    // setup translations
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useClass: TranslateMessageFormatCompiler
+      }
+    })
   ],
-  providers: [Title],
+  providers: [Title, { provide: MESSAGE_FORMAT_CONFIG, useValue: { locales: ['en', 'nl'] } }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
