8457ce4125db60d6ae18d0a538af6114fa0bdb83
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2019-08-12 13:12:47 +0200

    Experimenting with browser stack browsers.

--- enigmatry-blueprint-app-tests/test-caffe-runner.js
+++ enigmatry-blueprint-app-tests/test-caffe-runner.js
@@ -4,26 +4,10 @@ const minimist = require('minimist');
 // https://itnext.io/using-testcafe-with-browserstack-ea180520feb
 
 /*/ Each sub array defines a batch of browserstack workers.
- Our current plan allows for a max of 5 workers at a time,
- so to avoid crashing browserstack we group our browsers
- into 2 batches that run will run consecutively. /*/
+ into N batches that run will run consecutively. /*/
 const SUPPORTED_BROWSERS = [
   [
-    "firefox",
-    "browserstack:firefox@68.0:OS X Mojave",
-    "browserstack:firefox@67.0:OS X Mojave",
-    "browserstack:firefox@66.0:OS X Mojave",
-    "browserstack:firefox@68.0:Windows 10",
-    "browserstack:firefox@67.0:Windows 10",
-    "browserstack:firefox@66.0:Windows 10",
-  ],
-  [
-    "chrome",
-    "browserstack:chrome@75.0:OS X Mojave",
-    "browserstack:chrome@74.0:OS X Mojave",
-    "browserstack:chrome@73.0:OS X Mojave",
-    "browserstack:chrome@76.0:Windows 10",
-    "browserstack:chrome@75.0:Windows 10"
+    "browserstack:firefox",
   ]
 ];
 
