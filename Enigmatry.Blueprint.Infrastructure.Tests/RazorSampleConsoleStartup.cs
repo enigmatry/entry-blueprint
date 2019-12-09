c44c1d77bce7be30343579e1ca63e1d4604ed369
Srdjan Zivojinovic <srdjan.zivojinovic@modoolar.com>
2019-12-09 06:57:59 +0100

    BP-72: Added support for using Razor as templating engine outside of MVC (e.g. in console apps) in .NET Core 3.1 Removed old solution that worked with .NET Core 2.x only.

---
+++ Enigmatry.Blueprint.Infrastructure.Tests/RazorSampleConsoleStartup.cs
@@ -0,0 +1,40 @@
+﻿using Enigmatry.Blueprint.Infrastructure.Templating;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace Enigmatry.Blueprint.Infrastructure.Tests
+{
+    // example startup class - show how to initialize Razor in console application
+    // and RazorTemplatingEngine class. This class can be used for email templating purposes.
+    public class RazorSampleConsoleStartup
+    {
+        private readonly IConfiguration _configuration;
+
+        public RazorSampleConsoleStartup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string appDirectory = _configuration.GetValue<string>("App:RazorViewsRootDirectory");
+            services.AddRazorPages().AddRazorRuntimeCompilation(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(new PhysicalFileProvider(appDirectory));
+            });
+
+            services.AddSingleton<RazorTemplatingEngine>();
+        }
+
+        [UsedImplicitly]
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
--- Enigmatry.Blueprint.Infrastructure.Tests/Templating/RazorTemplatingEngineFixture.cs
+++ Enigmatry.Blueprint.Infrastructure.Tests/Templating/RazorTemplatingEngineFixture.cs
@@ -1,87 +1,61 @@
-﻿using System.Diagnostics;
-
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 using Enigmatry.Blueprint.Infrastructure.Templating;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Hosting.Internal;
-using Microsoft.Extensions.ObjectPool;
-using Microsoft.Extensions.PlatformAbstractions;
 using NUnit.Framework;
 
 namespace Enigmatry.Blueprint.Infrastructure.Tests.Templating
 {
-    // Sample test that shows how to initialize RazorViewEngine and MVC in order to enable email templating capabilities from
-    // non web types of application (e.g. console apps)
-    // https://devblogs.microsoft.com/aspnet/testing-asp-net-core-mvc-web-apps-in-memory/
-    // https://github.com/aspnet/Razor/issues/1212
-    // https://codeopinion.com/using-razor-in-a-console-application/
     [Category("unit")]
-    [Explicit]
     public class RazorTemplatingEngineFixture
     {
-#pragma warning disable CS8618 // It is initialized in Setup
-        private IServiceScopeFactory _scopeFactory;
-#pragma warning restore CS8618
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+        private RazorTemplatingEngine _templatingEngine;
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
         [SetUp]
         public void Setup()
         {
-            var services = new ServiceCollection();
-            ServiceProvider serviceProvider = ConfigureDefaultServices(services);
-            _scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+            IHost host = BuildHost();
+            IServiceScopeFactory scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+            IServiceScope serviceScope = scopeFactory.CreateScope();
+            _templatingEngine = serviceScope.ServiceProvider.GetRequiredService<RazorTemplatingEngine>();
         }
 
         [Test]
         public async Task TestRenderFromFile()
         {
-            using IServiceScope serviceScope = _scopeFactory.CreateScope();
-            var engine = serviceScope.ServiceProvider.GetRequiredService<RazorTemplatingEngine>();
+            string result = await _templatingEngine.RenderFromFileAsync("~/Templating/Sample.cshtml",
+                new EmailModel {SampleText = "Hello world!"});
 
-            string result = await engine.RenderFromFileAsync("~/Templating/Sample.cshtml", new EmailModel
-                {SampleText = "Hello world!"});
-               
             result.Should().Contain("Hello world!");
             result.Should().Contain("Congratulations!");
         }
 
-        private ServiceProvider ConfigureDefaultServices(ServiceCollection services)
+        private static IHost BuildHost()
         {
-            ApplicationEnvironment applicationEnvironment = PlatformServices.Default.Application;
-            services.AddSingleton(applicationEnvironment);
+            IConfiguration configuration = BuildConfiguration();
+            return Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<RazorSampleConsoleStartup>()
+                        .UseConfiguration(configuration);
+                }).Build();
+        }
 
-            string appDirectory = Directory.GetCurrentDirectory();
+        private static IConfiguration BuildConfiguration()
+        {
+            var rootDirectory = Directory.GetCurrentDirectory();
+            var configValues = new Dictionary<string, string> {{"App:RazorViewsRootDirectory", rootDirectory}};
 
-            services.AddMvc();
-            services.AddRazorPages();
-
-            var environment = new HostingEnvironment
-            {
-                ApplicationName = Assembly.GetEntryAssembly()?.GetName().Name
-            };
-            services.AddSingleton<IHostEnvironment>(serviceProvider => environment);
-
-            services.Configure<RazorViewEngineOptions>(options =>
-            {
-              //  options.FileProviders.Clear();
-              //  options.FileProviders.Add(new PhysicalFileProvider(appDirectory));
-            });
-
-            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
-
-            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
-            services.AddSingleton<DiagnosticSource>(diagnosticSource);
-
-            services.AddLogging();
-
-            services.AddSingleton<RazorTemplatingEngine>();
-            ServiceProvider provider = services.BuildServiceProvider();
-            return provider;
+            return new ConfigurationBuilder()
+                .AddInMemoryCollection(configValues)
+                .Build();
         }
     }
 }
