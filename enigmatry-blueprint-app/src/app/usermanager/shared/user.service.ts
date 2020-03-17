d778c530bd5b36d327f68b5b63980261b33d8ed0
Edwin de Koning <e.dekoning@enigmatry.com>
2020-03-17 14:58:35 +0100

    Updated CSP header Improved Response mapping in Angular app

--- enigmatry-blueprint-app/src/app/usermanager/shared/user.service.ts
+++ enigmatry-blueprint-app/src/app/usermanager/shared/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from './user.model';
 
 @Injectable({
@@ -12,7 +13,9 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   public getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(environment.apiUrl + '/users');
+    return this.httpClient.get<any>(environment.apiUrl + '/users').pipe<User[]>(
+      map(p => p.items)
+    );
   }
 
   public updateUser(user: User): Observable<User> {
@@ -22,7 +25,6 @@ export class UserService {
   }
 
   userById(id: string): Observable<User> {
-
     return this.httpClient.get<User>(environment.apiUrl + '/users/' + id);
   }
 
