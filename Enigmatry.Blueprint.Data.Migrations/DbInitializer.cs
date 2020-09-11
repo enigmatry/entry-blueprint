df92df8a688f24043a5ddf35757208c7c1ad0478
Edwin de Koning <eko@enigmatry.com>
2020-09-11 15:03:58 +0200

    Added migrations

--- Enigmatry.Blueprint.Data.Migrations/DbInitializer.cs
+++ Enigmatry.Blueprint.Data.Migrations/DbInitializer.cs
@@ -10,6 +10,7 @@ namespace Enigmatry.Blueprint.Data.Migrations
 
         static DbInitializer()
         {
+            Seedings.Add(new UserSeeding());
         }
 
         // EF Core way of seeding data: https://docs.microsoft.com/en-us/ef/core/modeling/data-seeding
