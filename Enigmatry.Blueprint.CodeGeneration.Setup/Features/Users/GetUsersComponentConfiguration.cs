65039c94f49e83d58978c0a1f47344c01c413c0f
Miljan Sljivic <miljan.sljivic@novalite.rs>
2021-04-28 09:41:24 +0200

    Code generation sample component configuration.

---
+++ Enigmatry.Blueprint.CodeGeneration.Setup/Features/Users/GetUsersComponentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Enigmatry.Blueprint.Api.Features.Users;
+using Enigmatry.Blueprint.CodeGeneration.Configuration.List;
+using JetBrains.Annotations;
+
+namespace Enigmatry.Blueprint.CodeGeneration.Setup.Features.Users
+{
+    [UsedImplicitly]
+    public class GetUsersComponentConfiguration : IListComponentConfiguration<GetUsers.Response.Item>
+    {
+        public void Configure(ListComponentBuilder<GetUsers.Response.Item> builder)
+        {
+            builder.Component()
+                .HasName("UserList")
+                .BelongsToFeature("Users");
+        }
+    }
+}
