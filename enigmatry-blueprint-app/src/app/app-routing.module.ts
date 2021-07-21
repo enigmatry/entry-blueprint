babb7e4c1faa56940dc6cadcd8f277758e2b3d46
Vladan Sljivic <vsl@enigmatry.com>
2021-07-21 06:05:11 +0000

    Merged PR 112: Disabling angular optimisation for CSS inlining because it breaks CSP
    https://github.com/angular/angular-cli/issues/20864

--- enigmatry-blueprint-app/src/app/app-routing.module.ts
+++ enigmatry-blueprint-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
   {
     path: 'users',
     loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule)
