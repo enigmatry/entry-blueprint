d6444b6603fbece7117fee8c40b7d13152c67f93
Miljan Sljivic <miljan.sljivic@novalite.rs>
2021-07-22 19:08:45 +0200

    Update code generation setup to latest version.

--- Enigmatry.Blueprint.CodeGeneration.Setup/Features/Users/UserEditComponentConfiguration.cs
+++ Enigmatry.Blueprint.CodeGeneration.Setup/Features/Users/UserEditComponentConfiguration.cs
@@ -1,29 +1,21 @@
-﻿using Enigmatry.Blueprint.Model.Identity.Commands;
+﻿using Enigmatry.Blueprint.Api.Features.Users;
 using Enigmatry.CodeGeneration.Configuration.Form;
 using JetBrains.Annotations;
 
 namespace Enigmatry.Blueprint.CodeGeneration.Setup.Features.Users
 {
     [UsedImplicitly]
-    internal class UserEditComponentConfiguration : IFormComponentConfiguration<UserCreateOrUpdate.Command>
+    public class UserEditComponentConfiguration : IFormComponentConfiguration<GetUserDetails.Response>
     {
-        public void Configure(FormComponentBuilder<UserCreateOrUpdate.Command> builder)
+        public void Configure(FormComponentBuilder<GetUserDetails.Response> builder)
         {
             builder.Component()
                 .HasName("UserEdit")
                 .BelongsToFeature("Users");
 
-            builder.HasCreateOrUpdateCommandWithName(nameof(UserCreateOrUpdate.Command));
-
-            builder.FormControl(x => x.Id).IsVisible(false);
-
-            builder.FormControl(x => x.UserName)
-                .WithLabel("UserName")
-                .WithPlaceholder("UserName");
-
-            builder.FormControl(x => x.Name)
-                .WithLabel("Name")
-                .WithPlaceholder("Name");
+            builder.FormControl(x => x.UserName).IsReadonly(true);
+            builder.FormControl(x => x.CreatedOn).IsReadonly(true);
+            builder.FormControl(x => x.UpdatedOn).IsReadonly(true);
         }
     }
 }
