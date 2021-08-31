e0bd42b19822b3bb3432dac597cb04277dc9612e
Edwin de Koning <e.dekoning@enigmatry.com>
2021-08-31 12:50:06 +0000

    Merged PR 310: BP-173: Remove Migration Runner from Blueprint template
    BP-173: Remove Migration Runner from Blueprint template

--- Enigmatry.Blueprint.Data.Migrations/Seeding/BlueprintContextFactory.cs
+++ Enigmatry.Blueprint.Data.Migrations/Seeding/BlueprintContextFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Principal;
 using Enigmatry.Blueprint.ApplicationServices.Identity;
 using Enigmatry.Blueprint.Infrastructure.Data;
@@ -15,40 +16,31 @@ namespace Enigmatry.Blueprint.Data.Migrations.Seeding
     [UsedImplicitly]
     public class BlueprintContextFactory : IDesignTimeDbContextFactory<BlueprintContext>
     {
-        // reading Environment variables because arguments cannot be passed in
-        // https://github.com/aspnet/EntityFrameworkCore/issues/8332
-        public BlueprintContext CreateDbContext(string[] args) => CreateDbContext(ReadConnectionString());
+        public BlueprintContext CreateDbContext(string[] args) => CreateDbContext(ReadConnectionString(args));
 
-        public BlueprintContext CreateDbContext(string connectionString)
+        private BlueprintContext CreateDbContext(string connectionString)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BlueprintContext>();
             optionsBuilder.UseSqlServer(connectionString,
-                b => b.MigrationsAssembly("Enigmatry.Blueprint.Data.Migrations"));
+                b => b.MigrationsAssembly(typeof(BlueprintContextFactory).Assembly.FullName));
 
             var result =
                 new BlueprintContext(optionsBuilder.Options, new NullMediator(), new TimeProvider(), new CurrentUserIdProvider(CreateEmptyPrincipal), new NullLogger<BlueprintContext>(), new NullDbContextAccessTokenProvider()) { ModelBuilderConfigurator = DbInitializer.SeedData };
             return result;
         }
 
-        // reading Environment variables because arguments cannot be passed in
-        // https://github.com/aspnet/EntityFrameworkCore/issues/8332
-        private static string ReadConnectionString()
+        private IPrincipal CreateEmptyPrincipal() => new GenericPrincipal(new GenericIdentity(""), Array.Empty<string>());
+
+        private static string ReadConnectionString(string[] args)
         {
-            const string environmentVariableName = "MigrateDatabaseConnectionString";
-            var connectionString = Environment.GetEnvironmentVariable(environmentVariableName);
-            Console.WriteLine(String.IsNullOrEmpty(connectionString)
-                ? $"{environmentVariableName} environment variable is not set."
-                : $"{environmentVariableName} variable was found. ");
+            var connectionString = args.FirstOrDefault();
 
             if (String.IsNullOrEmpty(connectionString))
             {
-                Console.WriteLine($"Falling back to developers connection string: '{DevelopmentConnectionsStrings.MainConnectionString}'");
-                return DevelopmentConnectionsStrings.MainConnectionString;
+                Console.WriteLine($"Connection string is not provided in the arguments. Falling back to developers connection string: '{DevelopmentConnectionsStrings.MainConnectionString}'");
+                connectionString = DevelopmentConnectionsStrings.MainConnectionString;
             }
-
             return connectionString;
         }
-
-        private IPrincipal CreateEmptyPrincipal() => new GenericPrincipal(new GenericIdentity(""), Array.Empty<string>());
     }
 }
