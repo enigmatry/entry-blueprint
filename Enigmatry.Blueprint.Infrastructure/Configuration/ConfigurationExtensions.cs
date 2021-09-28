58527ceffa7c45e8d663e7645c57045f4e8ba67e
Edwin de Koning <e.dekoning@enigmatry.com>
2021-09-28 11:47:37 +0000

    Merged PR 479: BP-114: Rewrite HealthChecks
    - Moved healthChecks to Enigmatry Building Blocks
    - Updated nuGet packages

--- Enigmatry.Blueprint.Infrastructure/Configuration/ConfigurationExtensions.cs
+++ Enigmatry.Blueprint.Infrastructure/Configuration/ConfigurationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Enigmatry.Blueprint.Core.Settings;
+using Enigmatry.BuildingBlocks.Core.Settings;
 using Microsoft.Extensions.Configuration;
 
 namespace Enigmatry.Blueprint.Infrastructure.Configuration
@@ -8,15 +9,13 @@ namespace Enigmatry.Blueprint.Infrastructure.Configuration
     {
         public static bool AppUseDeveloperExceptionPage(this IConfiguration configuration) => configuration.GetValue("UseDeveloperExceptionPage", false);
 
-        public static AppSettings ReadAppSettings(this IConfiguration configuration)
-        {
-            return configuration.ReadSettingsSection<AppSettings>("App");
-        }
+        public static AppSettings ReadAppSettings(this IConfiguration configuration) => configuration.ReadSettingsSection<AppSettings>("App");
 
-        public static ApplicationInsightsSettings ReadApplicationInsightsSettings(this IConfiguration configuration)
-        {
-            return configuration.ReadSettingsSection<ApplicationInsightsSettings>("ApplicationInsights");
-        }
+        public static ApplicationInsightsSettings ReadApplicationInsightsSettings(this IConfiguration configuration) =>
+            configuration.ReadSettingsSection<ApplicationInsightsSettings>(ApplicationInsightsSettings.ApplicationInsightsSectionName);
+
+        public static HealthCheckSettings ReadHealthCheckSettings(this IConfiguration configuration) =>
+            configuration.ReadSettingsSection<HealthCheckSettings>(HealthCheckSettings.HealthChecksSectionName);
 
         public static T ReadSettingsSection<T>(this IConfiguration configuration, string sectionName)
         {
