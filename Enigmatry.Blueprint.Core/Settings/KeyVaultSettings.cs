3e0de0823856344295f79e7e70b03236a6314fad
Edwin de Koning <e.dekoning@enigmatry.com>
2022-08-12 12:18:05 +0000

    Merged PR 4158: Updates for keyvault and configuration
    Moved Keyvault Configuration Extension method
    Added optional prefix parameter for environment variables to ConfigurationHelper

--- Enigmatry.Blueprint.Core/Settings/KeyVaultSettings.cs
+++ Enigmatry.Blueprint.Core/Settings/KeyVaultSettings.cs
@@ -5,6 +5,8 @@ namespace Enigmatry.Blueprint.Core.Settings
     [UsedImplicitly]
     public class KeyVaultSettings
     {
+        public const string SectionName = "KeyVault";
+
         public bool Enabled { get; set; }
         public string Name { get; set; } = String.Empty;
     }
---
+++ Enigmatry.Blueprint.Infrastructure/Init/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Identity;
+using Enigmatry.Blueprint.Infrastructure.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Enigmatry.Blueprint.Infrastructure.Init;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AppAddAzureKeyVault(this IConfigurationBuilder builder, string? keyVaultUri)
+    {
+        if (keyVaultUri != null)
+        {
+            builder.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+        }
+        return builder;
+    }
+
+    public static void AppAddAzureKeyVault(this IConfigurationBuilder builder, IConfiguration configuration)
+    {
+        var settings = configuration.ReadKeyVaultSettings();
+
+        if (settings.Enabled)
+        {
+            builder.AppAddAzureKeyVault($@"https://{settings.Name}.vault.azure.net");
+        }
+    }
+}
