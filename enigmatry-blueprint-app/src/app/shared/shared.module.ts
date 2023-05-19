0425db7dfcaa47c96176cf4aef4e346df0001e3e
Miljan Šljivić <miljan.sljivic@novalite.rs>
2023-05-19 15:11:03 +0200

    Add initial implementation of entry header component.

--- enigmatry-blueprint-app/src/app/shared/shared.module.ts
+++ enigmatry-blueprint-app/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { GridCellsModule } from './grid-cells/grid-cells.module';
 import { DEFAULT_DATE_FORMAT, DEFAULT_TIMEZONE, EntryTableModule } from '@enigmatry/entry-table';
 import { FormlyExtensionsModule } from '../formly/formly-extensions.module';
 import { FormWrapperComponent } from './form-wrapper/form-wrapper.component';
+import { EntryHeaderModule } from './entry-header/entry-header.module';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { FormWrapperComponent } from './form-wrapper/form-wrapper.component';
     GridCellsModule,
     EntryTableModule,
     FormlyExtensionsModule,
-    FormWrapperComponent
+    FormWrapperComponent,
+    EntryHeaderModule
   ],
   providers: [
     {
