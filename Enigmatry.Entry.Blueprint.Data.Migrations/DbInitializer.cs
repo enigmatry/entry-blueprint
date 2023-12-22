fa31cab9f02355f36ddd8c1c3d05cc7f48d85309
Andries van Waas <a.vanwaas@enigmatry.com>
2023-12-22 16:08:45 +0100

    https://enigmatry.atlassian.net/browse/BP-759 As a product owner I want the blueprint template to be updated to .NET 8 so it's on the latest LTS version for .NET.

--- Enigmatry.Entry.Blueprint.Data.Migrations/DbInitializer.cs
+++ Enigmatry.Entry.Blueprint.Data.Migrations/DbInitializer.cs
@@ -5,10 +5,7 @@ namespace Enigmatry.Entry.Blueprint.Data.Migrations;
 
 public static class DbInitializer
 {
-    private static readonly List<ISeeding> Seeding = new()
-    {
-        new RolePermissionSeeding()
-    };
+    private static readonly List<ISeeding> Seeding = [new RolePermissionSeeding()];
 
     // EF Core way of seeding data: https://docs.microsoft.com/en-us/ef/core/modeling/data-seeding
     public static void SeedData(ModelBuilder modelBuilder) => Seeding.ForEach(seeding => seeding.Seed(modelBuilder));
