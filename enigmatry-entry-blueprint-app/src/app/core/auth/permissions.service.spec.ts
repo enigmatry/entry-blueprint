d8c181bcc89059648ee599f0f7d3ec4d5ba70020
Nikola Dragićević <nda@enigmatry.com>
2024-01-24 13:02:47 +0100

    Added tests for permission service.

---
+++ enigmatry-entry-blueprint-app/src/app/core/auth/permissions.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed } from '@angular/core/testing';
+import { PermissionId } from 'src/app/api/api-reference';
+import { mockClass } from 'src/testing/mocks/class-mocker';
+import { CurrentUserService } from '../services/current-user.service';
+import { PermissionService } from './permissions.service';
+import { UserProfile } from './user-profile';
+
+jest.mock('../services/current-user.service');
+beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [CurrentUserService, PermissionService] });
+});
+const arrangePermissionService = (user: UserProfile | null) => {
+    mockClass(CurrentUserService, () => Object({
+        currentUser: user
+    }));
+    return TestBed.inject(PermissionService);
+};
+
+describe.each([
+    { description: 'should forbid actions if there is no user', user: null, allowed: false },
+    {
+        description: 'should forbid actions if there is no adequate permission',
+        user: UserProfile.fromResponse({ permissions: [PermissionId.ProductsRead, PermissionId.UsersRead] }),
+        allowed: false
+    },
+    {
+        description: 'should allow actions if there are minimal permissions',
+        user: UserProfile.fromResponse({ permissions: [PermissionId.ProductsRead, PermissionId.ProductsWrite] }),
+        allowed: true
+    }
+])('Testing permissions service...', data => {
+    it(data.description, () => {
+        const service = arrangePermissionService(data.user);
+
+        const result = service.hasPermissions([PermissionId.ProductsWrite, PermissionId.UsersWrite]);
+
+        expect(result).toBe(data.allowed);
+    });
+});
