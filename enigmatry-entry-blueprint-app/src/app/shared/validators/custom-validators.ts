8516a33190b48d63082539499c4251eba7b67683
Nikola Dragićević <nda@enigmatry.com>
2024-01-25 09:24:22 +0100

    Added tests for custom validators.

--- enigmatry-entry-blueprint-app/src/app/shared/validators/custom-validators.ts
+++ enigmatry-entry-blueprint-app/src/app/shared/validators/custom-validators.ts
@@ -1,28 +1,29 @@
 import { UntypedFormControl } from '@angular/forms';
-import { firstValueFrom } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { ValidatorsService as CustomValidatorsService } from './validators.service';
 
 export { ValidatorsService as CustomValidatorsService } from './validators.service';
 
-const productCodeIsUniqueValidator = async(control: UntypedFormControl, service: CustomValidatorsService)
-    : Promise<{ productCodeIsUnique: boolean } | null> => {
+const validate = async<T>(control: UntypedFormControl,
+    callback: (productId: string, value: string) => Observable<{ isUnique?: boolean | undefined }>,
+    expectedResult: T) => {
     if (control.dirty) {
-        const productId = control?.parent?.get('id')?.value;
-        const response = await firstValueFrom(service.isCodeUnique(productId, control.value));
-        return response?.isUnique ? null : { productCodeIsUnique: true };
+        const productId = control.parent?.get('id')?.value;
+        const response = await firstValueFrom(callback(productId, control.value));
+        return response?.isUnique ? null : expectedResult;
     }
     return Promise.resolve(null);
 };
 
+const productCodeIsUniqueValidator = async(control: UntypedFormControl, service: CustomValidatorsService)
+    : Promise<{ productCodeIsUnique: boolean } | null> =>
+    // eslint-disable-next-line @typescript-eslint/return-await
+    await validate(control, service.isCodeUnique, { productCodeIsUnique: true });
+
 const productNameIsUniqueValidator = async(control: UntypedFormControl, service: CustomValidatorsService)
-    : Promise<{ productNameIsUnique: boolean } | null> => {
-    if (control.dirty) {
-        const productId = control?.parent?.get('id')?.value;
-        const response = await firstValueFrom(service.isNameUnique(productId, control.value));
-        return response?.isUnique ? null : { productNameIsUnique: true };
-    }
-    return Promise.resolve(null);
-};
+    : Promise<{ productNameIsUnique: boolean } | null> =>
+    // eslint-disable-next-line @typescript-eslint/return-await
+    await validate(control, service.isNameUnique, { productNameIsUnique: true });
 
 export const customValidatorsFactory = (service: CustomValidatorsService) => {
     return {
