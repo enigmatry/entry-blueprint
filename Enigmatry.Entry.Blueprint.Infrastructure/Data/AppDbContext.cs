6bc0b229c761d2ce491b5062212222dd47de88ce
Srdjan Zivojinovic <srdjan.zivojinovic@crafters.cloud>
2024-08-23 11:25:42 +0200

    BP-266: Enable registering smart enums by convention.

--- Enigmatry.Entry.Blueprint.Infrastructure/Data/AppDbContext.cs
+++ Enigmatry.Entry.Blueprint.Infrastructure/Data/AppDbContext.cs
@@ -1,19 +1,23 @@
 ﻿using Enigmatry.Entry.EntityFramework;
 using Enigmatry.Entry.Blueprint.Infrastructure.Api.Init;
-using Enigmatry.Entry.SmartEnums.EntityFramework;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using Enigmatry.Entry.SmartEnums.EntityFramework;
 
 namespace Enigmatry.Entry.Blueprint.Infrastructure.Data;
 
 [UsedImplicitly]
 public class AppDbContext(DbContextOptions options) : BaseDbContext(CreateOptions(), options)
 {
-    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
-    {
-        configurationBuilder.EntryConfigureSmartEnum();
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder) =>
         configurationBuilder.Properties<string>()
             .HaveMaxLength(255);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // first we need to build the model so that we can later configure the smart enums
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.EntryConfigureSmartEnums();
     }
 
     private static EntitiesDbContextOptions CreateOptions() =>
