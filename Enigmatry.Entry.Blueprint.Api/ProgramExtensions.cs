a998040f041ef3279303465b51ef44b9e837ca8f
Edwin de Koning <eko@enigmatry.com>
2024-09-05 16:11:45 +0200

    Fix build errors

--- Enigmatry.Entry.Blueprint.Api/ProgramExtensions.cs
+++ Enigmatry.Entry.Blueprint.Api/ProgramExtensions.cs
@@ -78,13 +78,13 @@ public static class ProgramExtensions
             {
                 if (context.File.Name != "index.html")
                 {
-                    context.Context.Response.Headers["Cache-Control"] = "public, max-age: 604800";
-                    context.Context.Response.Headers.Add("Expires", "604800");
+                    context.Context.Response.Headers.Append("Cache-Control", "public, max-age: 604800");
+                    context.Context.Response.Headers.Append("Expires", "604800");
                 }
                 else
                 {
-                    context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
-                    context.Context.Response.Headers.Add("Expires", "-1");
+                    context.Context.Response.Headers.Append("Cache-Control", "no-cache, no-store");
+                    context.Context.Response.Headers.Append("Expires", "-1");
                 }
             }
         });
