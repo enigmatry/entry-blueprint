8ab554a1287807d06f443c1eb4ca1f580b54a5bf
Miljan Šljivić <miljan.sljivic@novalite.rs>
2024-12-05 16:09:55 +0100

    Remove auth.module

--- enigmatry-entry-blueprint-app/src/app/core/core.module.ts
+++ enigmatry-entry-blueprint-app/src/app/core/core.module.ts
@@ -1,14 +1,10 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { TitleStrategy } from '@angular/router';
-import { AuthModule } from '@app/auth/auth.module';
 import { GlobalErrorHandler } from '@services/global-error-handler';
 import { PageTitleStrategy } from '@services/page-title-strategy';
 import { provideCurrencyCode, provideDatePipeOptions } from './i18n/localization';
 
 @NgModule({
-  imports: [
-    AuthModule.forRoot()
-  ],
   providers: [
     {
       provide: ErrorHandler,
