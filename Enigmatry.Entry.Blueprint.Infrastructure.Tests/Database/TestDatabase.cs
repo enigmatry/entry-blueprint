efef38d270d711cb4c01f1939bab645f306fd79c
Andries van Waas <a.vanwaas@enigmatry.com>
2025-11-12 17:01:32 +0100

    cleanup

--- Enigmatry.Entry.Blueprint.Infrastructure.Tests/Database/TestDatabase.cs
+++ Enigmatry.Entry.Blueprint.Infrastructure.Tests/Database/TestDatabase.cs
@@ -39,7 +39,7 @@ internal class TestDatabase
                         .WithCleanUp(true)
                         .Build();
 
-                    _container!.StartAsync().Wait();
+                    _container.StartAsync().Wait();
                 }
 
                 ConnectionString = _container.GetConnectionString();
