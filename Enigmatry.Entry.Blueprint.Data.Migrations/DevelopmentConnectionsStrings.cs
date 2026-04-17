685fbe0b2c6dcfe38507c0dcb2463343aa696e3b
Edwin de Koning <49068840+edekoning@users.noreply.github.com>
2026-04-17 12:53:51 +0200

    Upgrade to .net 10 and Entry 10 (#77)
    * Upgrade to .net 10 and Entry 10

--- Enigmatry.Entry.Blueprint.Data.Migrations/DevelopmentConnectionsStrings.cs
+++ Enigmatry.Entry.Blueprint.Data.Migrations/DevelopmentConnectionsStrings.cs
@@ -2,6 +2,8 @@
 
 public static class DevelopmentConnectionsStrings
 {
+#pragma warning disable IDE0032 // Use auto property
     private static readonly string DatabaseName = "Enigmatry.Entry.Blueprint".Replace(".", "-", StringComparison.InvariantCulture).ToLowerInvariant();
+#pragma warning restore IDE0032 // Use auto property
     public static string MainConnectionString => $"Server=.;Database={DatabaseName};Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
 }
